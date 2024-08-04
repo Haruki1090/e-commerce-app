5c255e54ac4874b9388173f6806bcbef8d2f0871
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-04 12:12:32 +0900

    🏷️ Productの型を定義

---
+++ app/types/types.ts
@@ -0,0 +1,11 @@
+type ProductType = {
+    id: number;
+    title: string;
+    description: string;
+    thumbnail: {url: string};
+    price: number;
+    createdAt: string;
+    updatedAt: string;
+};
+
+export type { ProductType };

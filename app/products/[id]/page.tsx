4cd28b2ad4d3d511f5ef608672da6ec6a2e922ef
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-04 21:47:27 +0900

    ✨ 購入APIを実装

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,32 @@
-import { getProductById } from '@/app/lib/microcms/client';
-import { ProductDetailProps } from '@/app/types/types';
-import Image from 'next/image';
-
+import { getProductById } from "@/app/lib/microcms/client";
+import { ProductDetailProps } from "@/app/types/types";
+import Image from "next/image";
 
 const ProductDetail = async ({ params }: ProductDetailProps) => {
   const product = await getProductById(params.id);
 
   const thumbnail = product.thumbnail[0];
+
+  const startCheckout = async () => {
+    try {
+      const response = await fetch(
+        `$process.env.NEXT_PUBLIC_API_URL}/checkout`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title: product.title,
+            price: product.price,
+            id: product.id,
+          }),
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="container mx-auto p-4">
@@ -23,7 +43,12 @@ const ProductDetail = async ({ params }: ProductDetailProps) => {
           <h1 className="text-2xl font-bold">{product.title}</h1>
           <p className="mt-2 text-lg text-slate-600">{product.description}</p>
           <p className="mt-2 text-md text-slate-700">¥{product.price}円</p>
-          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">購入する</button>
+          <button
+            onClick={startCheckout}
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            購入する
+          </button>
         </div>
       </div>
     </div>

e71327c7ba9388ed6e35b05420ddd5ea4628386a
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-04 13:54:19 +0900

    ✨ 個別ページの実装

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { getProductById } from '@/app/lib/microcms/client';
+import { ProductDetailProps } from '@/app/types/types';
+import Image from 'next/image';
+
+
+const ProductDetail = async ({ params }: ProductDetailProps) => {
+  const product = await getProductById(params.id);
+
+  const thumbnail = product.thumbnail[0];
+
+  return (
+    <div className="container mx-auto p-4">
+      <div className="flex flex-col items-center">
+        <Image
+          priority
+          src={thumbnail.url}
+          alt={product.title}
+          width={thumbnail.width}
+          height={thumbnail.height}
+          className="rounded-md"
+        />
+        <div className="px-4 py-4 bg-slate-100 rounded-md mt-4">
+          <h1 className="text-2xl font-bold">{product.title}</h1>
+          <p className="mt-2 text-lg text-slate-600">{product.description}</p>
+          <p className="mt-2 text-md text-slate-700">¥{product.price}円</p>
+          <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">購入する</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;

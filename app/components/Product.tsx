4916ced5e88bd42f4f804a0207cfe5a08601323a
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-04 13:54:35 +0900

    ✨ 画面遷移機能を追加

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,32 +1,30 @@
 "use client";
 import Image from "next/image";
-import { ProductType } from "../types/types";
+import Link from "next/link";
+import { ProductProps } from "../types/types";
 
-type ProductProps = {
-  product : ProductType;
-};
 
-const Product = ({product} : ProductProps) => {
+const Product = ({ product }: ProductProps) => {
+  const thumbnail = product.thumbnail[0];
+
   return (
-    <>
-      <div className="flex flex-col items-center m-4">
-        <a className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
-          <Image
-            priority
-            src={product.thumbnail.url}
-            alt={product.title}
-            width={450}
-            height={350}
-            className="rounded-t-md"
-          />
-          <div className="px-4 py-4 bg-slate-100 rounded-b-md">
-            <h2 className="text-lg font-semibold">{product.title}</h2>
-            <p className="mt-2 text-lg text-slate-600">{product.description}</p>
-            <p className="mt-2 text-md text-slate-700">¥{product.price}円</p>
-          </div>
-        </a>
-      </div>
-    </>
+    <div className="flex flex-col items-center m-4">
+      <Link href={`/products/${product.id}`} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <Image
+          priority
+          src={thumbnail.url}
+          alt={product.title}
+          width={thumbnail.width}
+          height={thumbnail.height}
+          className="rounded-t-md"
+        />
+        <div className="px-4 py-4 bg-slate-100 rounded-b-md">
+          <h2 className="text-lg font-semibold">{product.title}</h2>
+          <p className="mt-2 text-lg text-slate-600">{product.description}</p>
+          <p className="mt-2 text-md text-slate-700">¥{product.price}円</p>
+        </div>
+      </Link>
+    </div>
   );
 };
 

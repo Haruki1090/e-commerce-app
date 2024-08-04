0a6878edaa0ff0ba380732a58556ae78d470fee7
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-04 21:54:36 +0900

    🐛 リダイレクトURL

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -25,6 +25,7 @@ export async function POST(request: Request, response: Response) {
       success_url: `http;//localhost:3000/products/${id}/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `http://localhost:3000/products/${id}`,
     });
+    return NextResponse.json({checkout_url: session.url});
   } catch (err: any) {
     return NextResponse.json(err.message);
   }
